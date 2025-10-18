4a28d21201b9d20a0d1c27b14cda0f3b778a91bf
khizar <masthantech619@gmail.com>
2025-10-18 05:48:19 +0530

    roboshop script updated

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z00132742FQ1VTFPNLKWC" # replace with your ZONE ID
 DOMAIN_NAME="shaik.cloud" # replace with your domain
 
-for instance in ${INSTANCES[@]}
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0c26f52aa9e841505 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
